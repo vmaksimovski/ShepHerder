cce0a4aa93b6c0f4c2c841fab495c815be415ecd
Rowland <rowlandz@outlook.com>
2018-10-28 04:44:52 -0400

    Added work from Rowlands PC again

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,13 +1,9 @@
 package src;
 
-
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
-public class Renderer extends JPanel
-{
-
+public class Renderer extends JPanel{
 	private static final long serialVersionUID = 1L;
 
 	@Override
